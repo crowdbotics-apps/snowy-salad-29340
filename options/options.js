0f5326eec06a005eb98687bc700930b20d845ee8
Crowdbotics <team@crowdbotics.com>
2021-08-02 19:33:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "snowy_salad_29340Identifier",
+  url: "https://snowy_salad_29340Identifier.botics.co",
+  api: "https://snowy_salad_29340Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
